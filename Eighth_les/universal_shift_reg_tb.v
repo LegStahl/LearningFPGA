ce8f2076fcff92e1d4b484cf5afcc8ffa1e19789
Штеле Богдан Эдуардович <Work>
2025-10-10 10:51:39 +0300

    Testbecnh of eighth lab was modified

--- Eighth_les/universal_shift_reg_tb.v
+++ Eighth_les/universal_shift_reg_tb.v
@@ -18,12 +18,12 @@ module universal_shift_reg_tb #(parameter CLK_PERIOD = 5) ;
 	 universal_shift_reg test(.clk(clk), .rst_n(rst_n), .select(select), .p_din(p_din), .s_left_din(s_left_din), .s_right_din(s_right_din), .p_dout(p_dout));
 	 
 	 
-	 always begin
-		clk = 1'b0;
-		#(CLK_PERIOD/2);
-		clk = 1'b1;
-		#(CLK_PERIOD/2);
-	 end
+	always begin
+			clk = 1'b0;
+			#(CLK_PERIOD/2);
+			clk = 1'b1;
+			#(CLK_PERIOD/2);
+	end
 	 
 	 integer count_of_mistake = 0;
 	 initial begin 
@@ -33,19 +33,26 @@ module universal_shift_reg_tb #(parameter CLK_PERIOD = 5) ;
 		p_din = 4'b1100;
 		@(posedge clk);
 		rst_n = 1'd1;
-		select = 2'b00;
+		select = 2'b11;
+		$display("Load\n");
 		p_din = 4'b1100;
 		@(posedge clk);
-		
+		select = 2'b00;
+		$display("Hold\n");
 		etalon_out = 4'b1100;
 		p_din = 4'b0100;
 		@(posedge clk);
-		
+		select = 2'b00;
 		etalon_out = 4'b0100;
 		p_din = 4'b1001;
 		@(posedge clk);
-		
 		etalon_out = 4'b1001;
+		select = 2'b01;
+		s_right_din = 1'b1;
+		@(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		$display("Started counting\n");
 		$stop;
 	 end
 	 
